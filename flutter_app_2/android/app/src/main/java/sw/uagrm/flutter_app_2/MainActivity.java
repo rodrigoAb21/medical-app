26645eeb0ca0c9ffd10891346dc5343f2b856cd6
Nishant Sabbarwal <54791056+Nishant3007@users.noreply.github.com>
2020-09-05 01:51:28 +0530

    Update MainActivity.java

--- flutter_app_2/android/app/src/main/java/sw/uagrm/flutter_app_2/MainActivity.java
+++ flutter_app_2/android/app/src/main/java/sw/uagrm/flutter_app_2/MainActivity.java
@@ -3,11 +3,28 @@ package sw.uagrm.flutter_app_2;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+//   @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(savedInstanceState);
+//     GeneratedPluginRegistrant.registerWith(this);
+//   }
+// }
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
+                        // Your existing code
+                       super.onCreate(savedInstanceState);
+                      GeneratedPluginRegistrant.registerWith(this);
+                }
+        );
+    }
+ }
